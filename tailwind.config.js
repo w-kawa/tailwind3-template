ce6597c082187a70ebf245c9f705dccc2da87a73
Wojciech Kawa <kawa.wojciech@outlook.com>
2021-12-22 00:09:43 +0100

    initial commit - Tailwind CSS 3

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    content: ['./public/**/*.html']
+  },
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
